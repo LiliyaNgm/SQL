d1d888bb6f60fce3e2294ab8334bacdb68ca2bf4
Lili <nigmatullinal@gmail.com>
2023-08-11 21:06:55 +0300

    sql

---
+++ azamat.sql
@@ -0,0 +1,11 @@
+Create table Azamat (
+    Data date,
+    History int,
+    Social_science int,
+    Geography int,
+    Sit_ups int,
+    Push_up int
+);
+
+INSERT INTO Azamat (Data, History, Social_science, Geography, Sit_ups, Push_up)
+Values ('11.08.2023', 0, 44, 0, 20, 30);
---
+++ iulai.sql
@@ -0,0 +1,10 @@
+Create table Iulai (
+    Data date,
+    Math int,
+    Physics int,
+    Sit_ups int,
+    Push_up int
+);
+
+INSERT INTO Iulai (Data, Math, Physics, Sit_ups, Push_up)
+Values ('11.08.2023', 0, 0, 20, 30);
